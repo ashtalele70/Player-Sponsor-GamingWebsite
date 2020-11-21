707478f5086d8cb7481a8c8c21a2e2f3f6b3e641
mahenaz-sayyed <mahenaz.sayyed@sjsu.edu>
2020-11-20 23:14:48 -0800

    Delete logic

--- cmpe275_lab2/src/main/java/edu/sjsu/cmpe275/lab2/dao/OpponentDaoImpl.java
+++ cmpe275_lab2/src/main/java/edu/sjsu/cmpe275/lab2/dao/OpponentDaoImpl.java
@@ -1,20 +1,28 @@
 package edu.sjsu.cmpe275.lab2.dao;
 
+import java.math.BigInteger;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+
+import javax.persistence.EntityManagerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import edu.sjsu.cmpe275.lab2.exception.OpponentsDoNotExist;
+import edu.sjsu.cmpe275.lab2.exception.OpponentsExistException;
 import edu.sjsu.cmpe275.lab2.exception.PlayerNotFoundException;
 import edu.sjsu.cmpe275.lab2.model.Opponent;
 import edu.sjsu.cmpe275.lab2.model.Player;
-import edu.sjsu.cmpe275.lab2.model.Sponsor;
+
 
 @Repository
 public class OpponentDaoImpl implements OpponentDao {
 	
 	private EntityManager entityManager;
+	private EntityManagerFactory entityManagerFactory;
 	 
 	  @Autowired
 	  public OpponentDaoImpl(EntityManager theEntityManager) {
@@ -23,7 +31,31 @@ public class OpponentDaoImpl implements OpponentDao {
 
 	@Override
 	public void deleteOpponent(Long id1, Long id2) {
-		// TODO Auto-generated method stub
+
+		Player player1 = entityManager.find(Player.class, id1);
+		Player player2 = entityManager.find(Player.class, id2);
+	    if(player1 == null || player2 == null) throw new PlayerNotFoundException("Player not found");
+	    
+	    else {
+		List<Long> list = entityManager.createNativeQuery("Select player_ID FROM opponent WHERE player_ID=:scrip and Opponent_ID = :prdate")
+                .setParameter("scrip", id1)
+                .setParameter("prdate", id2).getResultList();
+		if(list.isEmpty()) {
+			throw new OpponentsDoNotExist("Opponents not found"); 
+			
+		}
+		else {
+			Query query = entityManager.createNativeQuery("delete  from opponent where player_id =:id1 and opponent_id=:id2")
+					.setParameter("id1", id1)
+					.setParameter("id2", id2);;
+			query.executeUpdate();
+			
+			Query query2 = entityManager.createNativeQuery("delete  from opponent where player_id =:id2 and opponent_id=:id1")
+					.setParameter("id1", id1)
+					.setParameter("id2", id2);;
+			query2.executeUpdate();
+		}
+	    }
 		
 	}
 
@@ -36,23 +68,28 @@ public class OpponentDaoImpl implements OpponentDao {
 	    
 	    else {
 	    	
-	    	Query query = entityManager.createQuery("select id from opponent where playerId =:id AND opponentId=:id2")
-	    			.setParameter("id", id1)
-	    			.setParameter("id2", id2);    
-	    	int a = query.getFirstResult();
-	    	if(a>0) ;
-	    	else {
-		Opponent opponent1 = new Opponent();
-		Opponent opponent2 = new Opponent();
-		
-		
-		opponent1.setPlayerId(id1);
-		opponent1.setOpponentId(id2);
-		opponent2.setPlayerId(id2);
-		opponent2.setOpponentId(id1);
-		Opponent dbOpponent1 = entityManager.merge(opponent1);
-		Opponent dbOpponent2 = entityManager.merge(opponent2);
 	    
+	    	
+	    	List<Long> list = entityManager.createNativeQuery("Select player_ID FROM opponent WHERE player_ID=:scrip and Opponent_ID = :prdate")
+                    .setParameter("scrip", id1)
+                    .setParameter("prdate", id2).getResultList();
+	    	if(list.isEmpty()) {
+	    		Opponent opponent1 = new Opponent();
+	    		Opponent opponent2 = new Opponent();
+	    		
+	    		
+	    		opponent1.setPlayerId(id1);
+	    		opponent1.setOpponentId(id2);
+	    		opponent2.setPlayerId(id2);
+	    		opponent2.setOpponentId(id1);
+	    		Opponent dbOpponent1 = entityManager.merge(opponent1);
+	    		Opponent dbOpponent2 = entityManager.merge(opponent2);
+	    	}
+	    	
+	    	else {
+		
+	    		
+	    		throw new OpponentsExistException("Opponents exist");
 	    }
 	    }
 		
